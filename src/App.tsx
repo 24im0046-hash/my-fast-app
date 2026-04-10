17dbe357b1510da830e78a298840f021b0a7ac52
24im0046-hash <24im0046@i-u.ac.jp>
2026-04-10 00:52:08 +0000

    feat: completed tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-blue-700 text-white h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">こんにちは</h1>
+    </div>
+  )
+}
+
+export default App
