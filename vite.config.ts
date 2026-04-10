38a28607f85ac62d670d9417cab254db8a3bfe19
24im0046-hash <24im0046@i-u.ac.jp>
2026-04-10 01:15:08 +0000

    fix: add base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
+  base: '/my-fast-app/', // ← ここを追加！必ずリポジトリ名と一致させてください
   plugins: [
     react(),
     tailwindcss(),
